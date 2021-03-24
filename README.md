f489f25c020c236f7bd5630d067f3823fb207d3f
lukasz <lukaszpoweska@yahoo.com>
2021-03-24 19:45:51 +0100

    Init spring boot project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # agh-io-schedules-backend
+
+<h3>Run Spring Boot App Locally</h3>
+
+```shell
+sudo docker build -f db.Dockerfile -t io_postgres .
+sudo docker run -p 5432:5432 io_postgres
+gradle bootRun
+```
---
+++ src/test/java/gameofthreads/schedules/SchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package gameofthreads.schedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
