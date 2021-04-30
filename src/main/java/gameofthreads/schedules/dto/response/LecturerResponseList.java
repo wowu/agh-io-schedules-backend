988a489b29be386c5fbd740de2496a725426e2f1
Lukasz <poweska@student.agh.edu.pl>
2021-04-30 12:42:50 +0200

    Add public access modifier in dto

--- src/main/java/gameofthreads/schedules/dto/response/LecturerResponseList.java
+++ src/main/java/gameofthreads/schedules/dto/response/LecturerResponseList.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public class LecturerResponseList implements Serializable {
-    final List<LecturerResponse> lecturers;
+    public final List<LecturerResponse> lecturers;
 
     public LecturerResponseList(List<LecturerResponse> lecturers) {
         this.lecturers = lecturers;
